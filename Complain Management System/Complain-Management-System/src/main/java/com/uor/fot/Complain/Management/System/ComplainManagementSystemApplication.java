febe190645da398d6563ccbd4b4d2be693a7c92e
Jamila Hasangi <102423170+JamilaHasangi@users.noreply.github.com>
2023-10-22 14:10:47 +0530

    add project files

---
+++ Complain Management System/Complain-Management-System/src/main/java/com/uor/fot/Complain/Management/System/ComplainManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.uor.fot.Complain.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplainManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplainManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Complain Management System/Complain-Management-System/src/test/java/com/uor/fot/Complain/Management/System/ComplainManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uor.fot.Complain.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplainManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
