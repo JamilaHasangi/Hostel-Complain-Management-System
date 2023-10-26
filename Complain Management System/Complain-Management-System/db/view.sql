bf5e0ee5efc40e885f4e4bcd8454b98bf6720f21
Jamila Hasangi <102423170+JamilaHasangi@users.noreply.github.com>
2023-10-26 09:45:00 +0530

    create view for complain and report

--- Complain Management System/Complain-Management-System/db/view.sql
+++ Complain Management System/Complain-Management-System/db/view.sql
@@ -1,3 +1,4 @@
+---create view for student
 CREATE OR REPLACE view view_student_info AS
 SELECT u.first_name,
        u.last_name,
@@ -14,3 +15,30 @@ SELECT u.first_name,
        s.room_id
 FROM hcms.`user` u,hcms.`student` s
 WHERE u.id = s.user_id AND u.role_id = 1;
+
+---create view for complaint
+CREATE OR REPLACE view view_complaint_info AS
+SELECT u.first_name,
+       u.last_name,
+       u.role_id,
+       u.faculty_id,
+       u.created_at,
+       u.updated_at,
+       cp.asset_id,
+       cp.user_id,
+       cp.description,
+       cp.submission_date,
+       cp. is_resolved,
+       cp. quantity
+FROM hcms.`user` u,hcms.`complaint` cp
+WHERE u.id = cp.id ;
+
+----create view for report
+CREATE OR REPLACE view view_complaint_info AS
+SELECT u.first_name,
+       u.last_name,
+       u.role_id,
+       r.content,
+       r.date_time
+FROM hcms.`user` u,hcms.`report` r
+WHERE u.id = r.user_id ;
