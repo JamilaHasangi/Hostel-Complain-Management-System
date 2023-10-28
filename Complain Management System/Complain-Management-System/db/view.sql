44e0e3f7709685107143e24a2944a9de5c43a1d4
hasaniam <102344424+hasaniam@users.noreply.github.com>
2023-10-28 10:09:49 +0530

    Update view.sql

--- Complain Management System/Complain-Management-System/db/view.sql
+++ Complain Management System/Complain-Management-System/db/view.sql
@@ -24,31 +24,31 @@ SELECT u.first_name,
        u.nic,
        u.contact,
        u.address,
-       u.role_id,
-       u.faculty_id,
+       ur.name AS role_name,
+       f.name AS faculty_name,
        u.created_at,
        u.updated_at,
        u.status,
        d.user_id
-FROM hcms.`user` u,hcms.`dean` d
-WHERE u.id = d.user_id AND u.role_id = 2;
+FROM hcms.`user` u,hcms.`dean` d, hcms.`user_role` ur, hcms.`faculty` f
+WHERE u.id = d.user_id AND u.role_id = 2 AND u.role_id = ur.id AND u.faculty_id = f.id;
 
 ---create view for complaint
 CREATE OR REPLACE view view_complaint_info AS
 SELECT u.first_name,
        u.last_name,
-       u.role_id,
-       u.faculty_id,
+       ur.name AS role_name,
+       f.name AS faculty_name,
        u.created_at,
        u.updated_at,
-       cp.asset_id,
+       a.name AS asset_name,
        cp.description,
        cp.submission_date,
        cp.is_resolved,
        cp.quantity,
        cp.status
-FROM hcms.`user` u,hcms.`complaint` cp
-WHERE u.id = cp.id ;
+FROM hcms.`user` u,hcms.`complaint` cp, hcms.`user_role` ur, hcms.`faculty` f, hcms.`asset` a
+WHERE u.id = cp.user_id AND u.role_id = ur.id AND u.faculty_id = f.id AND cp.asset_id = a.id;
 
 
 -- senior student counselor information
@@ -59,14 +59,14 @@ SELECT u.first_name,
        u.nic,
        u.contact,
        u.address,
-       u.role_id,
-       u.faculty_id,
+       ur.name AS role_name,
+       f.name AS faculty_name,
        u.created_at,
        u.updated_at,
        u.status,
        ss.user_id
-FROM hcms.`user` u,hcms.`senior_student_counselor` ss
-WHERE u.id = ss.user_id AND u.role_id = 5;
+FROM hcms.`user` u,hcms.`senior_student_counselor` ss, hcms.`user_role` ur, hcms.`faculty` f
+WHERE u.id = ss.user_id AND u.role_id = 5 AND u.role_id = ur.id AND u.faculty_id = f.id;
 
 -- sub warden information
 CREATE OR REPLACE view view_sub_warden_info AS
@@ -76,14 +76,14 @@ SELECT u.first_name,
        u.nic,
        u.contact,
        u.address,
-       u.role_id,
-       u.faculty_id,
+       ur.name AS role_name,
+       f.name AS faculty_name,
        u.created_at,
        u.updated_at,
        u.status,
        sw.user_id
-FROM hcms.`user` u,hcms.`sub_warden` sw
-WHERE u.id = sw.user_id AND u.role_id = 3;
+FROM hcms.`user` u,hcms.`sub_warden` sw, hcms.`user_role` ur, hcms.`faculty` f
+WHERE u.id = sw.user_id AND u.role_id = 3 AND u.role_id = ur.id AND u.faculty_id = f.id;
 
 
 -- academic warden information
@@ -94,23 +94,23 @@ SELECT u.first_name,
        u.nic,
        u.contact,
        u.address,
-       u.role_id,
-       u.faculty_id,
+       ur.name AS role_name,
+       f.name AS faculty_name,
        u.created_at,
        u.updated_at,
        u.status,
        aw.id
-FROM hcms.`user` u,hcms.`academic_warden` aw
-WHERE u.id = aw.id AND u.role_id = 4;
+FROM hcms.`user` u,hcms.`academic_warden` aw, hcms.`user_role` ur, hcms.`faculty` f
+WHERE u.id = aw.id AND u.role_id = 4 AND u.role_id = ur.id AND u.faculty_id = f.id;
 
 
-----create view for report
+-- create view for report
 CREATE OR REPLACE view view_report_info AS
 SELECT u.first_name,
        u.last_name,
-       u.role_id,
+       ur.name AS role_name,
        r.content,
        r.date_time
-FROM hcms.`user` u,hcms.`report` r
-WHERE u.id = r.user_id ;
+FROM hcms.`user` u,hcms.`report` r, hcms.`user_role` ur
+WHERE u.id = r.user_id AND u.role_id = ur.id;
 
