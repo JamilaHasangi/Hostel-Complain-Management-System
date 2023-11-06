8fc638818b80b1f1416fdc8b2ba39348912e35c3
Jamila Hasangi <102423170+JamilaHasangi@users.noreply.github.com>
2023-11-06 18:41:01 +0530

    interface update

--- Complain Management System/Complain-Management-System/db/index.sql
+++ Complain Management System/Complain-Management-System/db/index.sql
@@ -2,7 +2,7 @@
 CREATE INDEX index_email_address  ON user (email);
 CREATE INDEX index_nic  ON user (nic);
 CREATE INDEX index_name  ON asset (name);
-CREATE INDEX index_qr_code  ON complaint (qr_code_url);
+CREATE INDEX index_complaint_status  ON complaint (status);
 CREATE INDEX index_report_date  ON report (report_date);
 CREATE INDEX index_report_type  ON report (report_type);
 CREATE INDEX index_students_count  ON room (no_of_students);
@@ -19,6 +19,3 @@ CREATE INDEX index_condition_name ON asset (asset_condition, name);
 CREATE INDEX index_content_date ON report (content, date_time);
 CREATE INDEX index_description_isResolved ON complaint (description, is_resolved);
 
-
-CREATE UNIQUE INDEX index_name ON table_name (column_name);
-CREATE NONCLUSTERED INDEX index_name ON table_name (column_name);
